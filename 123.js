8ea884ddc050f10966108865c9b006170a9564a2
Степан04123 <leces2@mail.ru>
2022-11-03 12:14:06 +0700

    first

---
+++ 123.js
@@ -0,0 +1,12 @@
+let x1 = 2
+let y1 = 3
+
+let x2 = 10
+let y2 = 5
+
+let cathetus1 = Math.abs(x1 - x2);
+let cathetus2 = Math.abs(y1 - y2);
+
+console.log(Math.sqrt(
+  Math.pow(cathetus1, 2) * Math.pow(cathetus2, 2)
+));
